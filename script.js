b7cc48c5d7d9440cc7d99aba83e80ecad1f0f137
sophiayangx <sopyang@nuevaschool.org>
2016-12-07 09:33:36 -0800

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,28 @@
+$("body").keydown(function(keydownEvent) {
+  if (keydownEvent.keyCode === 13) {
+    goForward(100);
+    turnLeft();
+  } 
+})
+
+//var position = 200
+//
+////insertLine(200, 30, 200)
+
+function sides(distance, n) {
+  for (i = 0; i < n; i++) {
+    goForward(100)
+    turnLeft(n);
+  }
+}
+
+sides(3, 6)
+
+function sides(distance, n) {
+  for (i = 0; i < n; i++) {
+    goForward(100)
+    turnLeftStar(n);
+  }
+}
+
+sides(3, 6)
